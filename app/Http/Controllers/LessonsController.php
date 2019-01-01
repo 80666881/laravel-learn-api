ad3fafb71a615f4544d82ec016379d1957a57ed9
铁腕阿童木 <zhengzehao@maka.im>
2019-01-01 14:00:48 +0800

    add transform  method

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -7,24 +7,28 @@ use App\Lesson;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Response;
 
-class LessonsController extends Controller
-{
+class LessonsController extends Controller {
     //
-    public function index(){
+    public function index() {
         $lessons = Lesson::all();
         return Response::json([
-            'status'=>'success',
-            'status_code'=>200,
-            'data'=>$lessons->toArray()
-        ]);
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => $this->transform($lessons)]);
     }
 
-    public function show($id){
-        $lesson =  Lesson::findOrFail($id);
-        return Response::json([
-            'status'=>'success',
-            'status_code'=>200,
-            'data'=>$lesson->toArray()
-        ]);
+    public function show($id) {
+        $lesson = Lesson::findOrFail($id);
+        return Response::json(['status' => 'success', 'status_code' => 200, 'data' => $lesson->toArray()]);
+    }
+
+    private function transform($lessons) {
+        return array_map(function ($lesson) {
+            return [
+                'title' => $lesson['title'],
+                'content' => $lesson['body'],
+                'is_free' => (boolean)$lesson['free']
+            ];
+        }, $lessons->toArray());
     }
 }
